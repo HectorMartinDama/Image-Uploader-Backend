541547f1319e5b0c0926c3a7be7cd8d9e806f81a
Hector Martin <hectormartindama@gmail.com>
2023-07-03 11:20:07 +0200

    feat: new version

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
-import express from 'express'; 
-import multer from 'multer';
-import helmet from 'helmet';
-import http from 'http';
-import path from 'path';
-import cors  from 'cors';
-import { v4 as uuidv4 } from 'uuid';
+import express from 'express'
+import multer from 'multer'
+import helmet from 'helmet'
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import { v4 as uuidv4 } from 'uuid'
 import 'dotenv/config'
 
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    return callback(null, './uploads')
+  },
+  filename: function (req, file, callback) {
+    return callback(null, `${uuidv4()}${path.extname(file.originalname)}`)
+  }
+})
 
-const storage= multer.diskStorage({
-    destination: function(req, file, callback){
-        return callback(null, './uploads');
-    },
-    filename: function(req, file, callback){
-        return callback(null, `${uuidv4()}${path.extname(file.originalname)}`);
-    }
-});
+const upload = multer({ storage })
+const PORT = process.env.PORT || 5000
+const app = express()
 
-const upload = multer({storage: storage});  
-const PORT= process.env.PORT || 5000; 
-const app= express();
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+const server = http.createServer(app)
 
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-const server= http.createServer(app); 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' })
+})
 
-app.get('/health', (req, res) =>{
-    res.json({status: 'ok'});
-});
+app.post('/', upload.single('image'), (req, res) => {
+  if (!req.file) return res.send({ success: false })
+  else return res.json({ success: true, fileName: req.file.filename })
+})
 
-app.post('/', upload.single('image'), (req, res) =>{
-    if(!req.file) return res.send({success: false});
-    else return res.json({success: true, fileName: req.file.filename });
-});
+app.get('/image/:name', (req, res) => {
+  res.sendFile(path.resolve('./uploads/' + req.params.name))
+})
 
-app.get('/image/:name', (req, res) =>{
-    res.sendFile(path.resolve('./uploads/' + req.params.name));
-});
+app.use((req, res) => {
+  res.status(404).send('This endpoint does not exists')
+})
 
-app.use((req, res) =>{
-    res.status(404).send('This endpoint does not exists');
-});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
 
-server.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
-});
-
-export default app;
+export default app
--- test/upload.test.js
+++ test/upload.test.js
@@ -1,28 +1,29 @@
-import app from '../app.js';
-import supertest from "supertest";
-import fs from 'fs';
+import app from '../app.js'
+import supertest from 'supertest'
+import fs from 'fs'
 
-const api= supertest(app);
-let filePathName;
+import { test, afterAll, expect } from 'vitest'
+
+const api = supertest(app)
+let filePathName
 
 test('return status code 404, when the route does not exist', async () => {
-    await api.get('/dontExists').expect(404);
-});
+  await api.get('/dontExists').expect(404)
+})
 
-test('upload a image to the server', async () =>{
-    if(fs.existsSync('./test/testImage.jpeg')){
-        const response= await api.post('/').attach('image', './test/testImage.jpeg').expect(200);
-        expect(response.body.success).toBe(true);
-        expect(fs.existsSync(`./uploads/${response.body.fileName}`)).toBe(true);
-        filePathName=response.body.fileName;
-    }
-});
+test('upload a image to the server', async () => {
+  if (fs.existsSync('./test/testImage.jpeg')) {
+    const response = await api.post('/').attach('image', './test/testImage.jpeg').expect(200)
+    expect(response.body.success).toBe(true)
+    expect(fs.existsSync(`./uploads/${response.body.fileName}`)).toBe(true)
+    filePathName = response.body.fileName
+  }
+})
 
-test('get a image from the server', async () =>{
-    await api.get(`/image/${filePathName}`).expect('Content-Type', 'image/jpeg').expect(200);    
-});
+test('get a image from the server', async () => {
+  await api.get(`/image/${filePathName}`).expect('Content-Type', 'image/jpeg').expect(200)
+})
 
-
-afterAll(() =>{
-    fs.unlinkSync(`./uploads/${filePathName}`);
-});
+afterAll(() => {
+  fs.unlinkSync(`./uploads/${filePathName}`)
+})
